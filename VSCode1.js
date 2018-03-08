b6411cfecebf463d06a8ef5bf1e36d5d3ad4a554
Vishal-Kumar-1511 <37135976+Vishal-Kumar-1511@users.noreply.github.com>
2018-03-08 11:00:09 +0530

    Create VSCode1.js

---
+++ VSCode1.js
@@ -0,0 +1,15 @@
+console.log("Hey Fresh!!");
+
+setTimeout(function () {
+	console.log("3 seconds have passed");
+},3000);
+
+var time = 0;
+
+var timer = setInterval(function () {
+    time +=2 ;
+    console.log (time + " seconds have passed");
+    if(time > 5) {
+        clearInterval(timer);
+    }
+},3000);
